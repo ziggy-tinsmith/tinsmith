e232c3637c8d8a7f872121a49d4db6d387f653c1
altermarkive <altermarkive@users.noreply.github.com>
2020-12-19 15:19:23 +0000

    gh-pages

---
+++ precache-manifest.0ac88c88adb043c1ec6094c6799aa646.js
@@ -0,0 +1,510 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58d3683dbfce0b20187",
+    "url": "/tinsmith/css/chunk-vendors.16cca142.css"
+  },
+  {
+    "revision": "61728a42b7823f0fdc71",
+    "url": "/tinsmith/css/feature01~feature02~feature03~feature04a~feature04b~feature05~feature06~feature07~feature08a~feature~9e1b3846.53b09bcc.css"
+  },
+  {
+    "revision": "383d94e0d1cdc084f2e3",
+    "url": "/tinsmith/css/information.68e6a77c.css"
+  },
+  {
+    "revision": "541e65fb46e44ecdf7a9da8a9b881446",
+    "url": "/tinsmith/fonts/materialdesignicons-webfont.541e65fb.eot"
+  },
+  {
+    "revision": "c61b9c12f68ee1ba045a4b49dba29ca5",
+    "url": "/tinsmith/fonts/materialdesignicons-webfont.c61b9c12.woff2"
+  },
+  {
+    "revision": "fc03f7f15facede623faa7666c7d1f5a",
+    "url": "/tinsmith/fonts/materialdesignicons-webfont.fc03f7f1.ttf"
+  },
+  {
+    "revision": "ff13d121c1a1cf74d8e06bd39e1746c3",
+    "url": "/tinsmith/fonts/materialdesignicons-webfont.ff13d121.woff"
+  },
+  {
+    "revision": "9c19bc48e6da095cf04ac3e1f6d48221",
+    "url": "/tinsmith/img/feature01.9c19bc48.png"
+  },
+  {
+    "revision": "585b623de03f08317beca77d65c1a96b",
+    "url": "/tinsmith/img/feature02.585b623d.png"
+  },
+  {
+    "revision": "acf3a5e238c7f8b819c368941de2e140",
+    "url": "/tinsmith/img/feature03.acf3a5e2.png"
+  },
+  {
+    "revision": "eae15ef4486144d2dec42559033ed90f",
+    "url": "/tinsmith/img/feature04.eae15ef4.png"
+  },
+  {
+    "revision": "3f8984f583c3bbd3128961c1d0562739",
+    "url": "/tinsmith/img/feature05.3f8984f5.png"
+  },
+  {
+    "revision": "f7fca11fc0a79cd65f18c3e4ad1df5c4",
+    "url": "/tinsmith/img/feature06.f7fca11f.png"
+  },
+  {
+    "revision": "ff6a7f1655eab78449b2582ac4c15a54",
+    "url": "/tinsmith/img/feature07.ff6a7f16.png"
+  },
+  {
+    "revision": "df814162ae3502e5037a2c47d5815064",
+    "url": "/tinsmith/img/feature08.df814162.png"
+  },
+  {
+    "revision": "5deb95e95f39ff5c688a370ad5d13540",
+    "url": "/tinsmith/img/feature09.5deb95e9.png"
+  },
+  {
+    "revision": "126ad0d1254a62886ff2617522d5ed2a",
+    "url": "/tinsmith/img/feature10.126ad0d1.png"
+  },
+  {
+    "revision": "4c10060a6543fc6a274644cebc2321e7",
+    "url": "/tinsmith/img/feature11.4c10060a.png"
+  },
+  {
+    "revision": "585b623de03f08317beca77d65c1a96b",
+    "url": "/tinsmith/img/feature12.585b623d.png"
+  },
+  {
+    "revision": "29b4fae7085730f273f19d07ce445c43",
+    "url": "/tinsmith/img/feature13.29b4fae7.png"
+  },
+  {
+    "revision": "329b525be8819b845768dd8278e40fbd",
+    "url": "/tinsmith/img/feature14.329b525b.png"
+  },
+  {
+    "revision": "bc46d44af66932a059a1048faacefd6d",
+    "url": "/tinsmith/img/feature15.bc46d44a.png"
+  },
+  {
+    "revision": "b0ea20698ecdf1258388e0799d419877",
+    "url": "/tinsmith/img/feature16.b0ea2069.png"
+  },
+  {
+    "revision": "3e025605aee918724ba48d6b0bbeb1bf",
+    "url": "/tinsmith/img/feature17.3e025605.png"
+  },
+  {
+    "revision": "a4d622d5e406b9ef6ae5a302f534aa19",
+    "url": "/tinsmith/img/feature18.a4d622d5.png"
+  },
+  {
+    "revision": "80f971002c9fbdeb02966b807ae68e04",
+    "url": "/tinsmith/img/feature19.80f97100.png"
+  },
+  {
+    "revision": "534b3e702326857eb4c8e54ea99a6b50",
+    "url": "/tinsmith/img/feature20.534b3e70.png"
+  },
+  {
+    "revision": "7f74691425e54c8858ffbfbd5c06ea46",
+    "url": "/tinsmith/img/feature21.7f746914.png"
+  },
+  {
+    "revision": "60b4b9f1c842effa9f55de6b7854b368",
+    "url": "/tinsmith/img/feature22.60b4b9f1.png"
+  },
+  {
+    "revision": "585b623de03f08317beca77d65c1a96b",
+    "url": "/tinsmith/img/feature23.585b623d.png"
+  },
+  {
+    "revision": "71f9335369ead994d58c08cb32643c04",
+    "url": "/tinsmith/img/feature24.71f93353.png"
+  },
+  {
+    "revision": "1e2ef56af0d2c51d692a706516126f93",
+    "url": "/tinsmith/img/feature25.1e2ef56a.png"
+  },
+  {
+    "revision": "585b623de03f08317beca77d65c1a96b",
+    "url": "/tinsmith/img/feature26.585b623d.png"
+  },
+  {
+    "revision": "fa20077cb5d171e865c6b6ed734fbccc",
+    "url": "/tinsmith/img/feature27.fa20077c.png"
+  },
+  {
+    "revision": "d6d2ee409002b016d1fdb1111682d281",
+    "url": "/tinsmith/img/feature28.d6d2ee40.png"
+  },
+  {
+    "revision": "78af1208878ce6e2dd0494a2417e1cf0",
+    "url": "/tinsmith/img/feature29.78af1208.png"
+  },
+  {
+    "revision": "585b623de03f08317beca77d65c1a96b",
+    "url": "/tinsmith/img/feature30.585b623d.png"
+  },
+  {
+    "revision": "60797050bcc9440a5e84caab42257890",
+    "url": "/tinsmith/img/feature31.60797050.png"
+  },
+  {
+    "revision": "ff3b4db9031f29d5a311bddf86df7213",
+    "url": "/tinsmith/img/feature32.ff3b4db9.png"
+  },
+  {
+    "revision": "1950ce3e1fa7605b9fc3b02e2a06ca71",
+    "url": "/tinsmith/img/feature33.1950ce3e.png"
+  },
+  {
+    "revision": "410a0776f22a2f63854567c5892b9125",
+    "url": "/tinsmith/img/feature34.410a0776.png"
+  },
+  {
+    "revision": "80002ff11312effeb395d0a755a11666",
+    "url": "/tinsmith/img/feature35.80002ff1.png"
+  },
+  {
+    "revision": "fcbc585c86ca09f366404b361b7bdbe8",
+    "url": "/tinsmith/img/feature39.fcbc585c.png"
+  },
+  {
+    "revision": "c779b7c406d4675b5df142b87009d77d",
+    "url": "/tinsmith/img/feature40.c779b7c4.png"
+  },
+  {
+    "revision": "cb8579ae7357128f4397ce8ed068ef31",
+    "url": "/tinsmith/img/feature41.cb8579ae.png"
+  },
+  {
+    "revision": "b8dbeb7c70bb15217fee75b23d4c51ea",
+    "url": "/tinsmith/img/feature43.b8dbeb7c.png"
+  },
+  {
+    "revision": "9447fa60ebeae3650e196c3a1281921f",
+    "url": "/tinsmith/img/feature44.9447fa60.png"
+  },
+  {
+    "revision": "fd9e927d4b1aacec168a40fb8637048c",
+    "url": "/tinsmith/img/feature45.fd9e927d.png"
+  },
+  {
+    "revision": "2cd6b3b7f2436026036fc0ddd19199c6",
+    "url": "/tinsmith/img/feature46.2cd6b3b7.png"
+  },
+  {
+    "revision": "1f001af92a5762ede0844d6c59a3629f",
+    "url": "/tinsmith/img/feature47.1f001af9.png"
+  },
+  {
+    "revision": "8b76b19bf8445a98d8f6bc50d7a44c11",
+    "url": "/tinsmith/img/feature48.8b76b19b.png"
+  },
+  {
+    "revision": "31f137ae94e8ba2ad15a1fa6335791b2",
+    "url": "/tinsmith/img/feature49.31f137ae.png"
+  },
+  {
+    "revision": "585b623de03f08317beca77d65c1a96b",
+    "url": "/tinsmith/img/feature50.585b623d.png"
+  },
+  {
+    "revision": "be200caad82a7020493700c2d5c7f2e5",
+    "url": "/tinsmith/img/feature51.be200caa.png"
+  },
+  {
+    "revision": "d45e5785e664ab94c2033bad118fbeba",
+    "url": "/tinsmith/img/feature52.d45e5785.png"
+  },
+  {
+    "revision": "cb3f8246c1da3a3b89a872a506db073a",
+    "url": "/tinsmith/img/feature53.cb3f8246.png"
+  },
+  {
+    "revision": "f30cb398914519e5d78eebe32bdd360a",
+    "url": "/tinsmith/img/feature54.f30cb398.png"
+  },
+  {
+    "revision": "4fb10fe86c87892ff8ef07e49a946383",
+    "url": "/tinsmith/img/feature55.4fb10fe8.png"
+  },
+  {
+    "revision": "5e10a97e080f744399606714c9eabf73",
+    "url": "/tinsmith/img/feature56.5e10a97e.png"
+  },
+  {
+    "revision": "eeddd27c369f581aadcd5df8fbd1a0d3",
+    "url": "/tinsmith/img/feature57.eeddd27c.png"
+  },
+  {
+    "revision": "12f5ff1b9534585145057b8e853a2ce8",
+    "url": "/tinsmith/img/logo.12f5ff1b.png"
+  },
+  {
+    "revision": "994d7b92aa8515c9db3d41ba5ab53277",
+    "url": "/tinsmith/img/logo.994d7b92.svg"
+  },
+  {
+    "revision": "9099e8f75e14600ae79833ed5318c01a",
+    "url": "/tinsmith/index.html"
+  },
+  {
+    "revision": "519b6e41b30099ef1a64",
+    "url": "/tinsmith/js/app.8eb6f12f.js"
+  },
+  {
+    "revision": "b58d3683dbfce0b20187",
+    "url": "/tinsmith/js/chunk-vendors.573e65bd.js"
+  },
+  {
+    "revision": "e31914dde55df2a10b10",
+    "url": "/tinsmith/js/feature01.c483f28e.js"
+  },
+  {
+    "revision": "61728a42b7823f0fdc71",
+    "url": "/tinsmith/js/feature01~feature02~feature03~feature04a~feature04b~feature05~feature06~feature07~feature08a~feature~9e1b3846.d9bd784f.js"
+  },
+  {
+    "revision": "40adee52e15d05c632de",
+    "url": "/tinsmith/js/feature02.7ab9d2d0.js"
+  },
+  {
+    "revision": "e5e4cf966574d54b759d",
+    "url": "/tinsmith/js/feature03.8257d0b5.js"
+  },
+  {
+    "revision": "0e6b2ee88d388e382024",
+    "url": "/tinsmith/js/feature04a.a3d2bedd.js"
+  },
+  {
+    "revision": "e45bd27b922e2276807a",
+    "url": "/tinsmith/js/feature04b.171e64c1.js"
+  },
+  {
+    "revision": "97aeba176c9f301b63fc",
+    "url": "/tinsmith/js/feature05.25e177e3.js"
+  },
+  {
+    "revision": "413d314556d502701ab8",
+    "url": "/tinsmith/js/feature06.fddd3ad2.js"
+  },
+  {
+    "revision": "44334ab8cbd3e8d41569",
+    "url": "/tinsmith/js/feature07.cbdf399a.js"
+  },
+  {
+    "revision": "f8b5ecfd85cab84919f0",
+    "url": "/tinsmith/js/feature08a.de28c70b.js"
+  },
+  {
+    "revision": "0979b1e5d5dec31b8da9",
+    "url": "/tinsmith/js/feature08b.eab88dd0.js"
+  },
+  {
+    "revision": "d836c4d82701325f084a",
+    "url": "/tinsmith/js/feature09a.0a8e27a4.js"
+  },
+  {
+    "revision": "e1e142485aa53247c321",
+    "url": "/tinsmith/js/feature09b.53de8b8f.js"
+  },
+  {
+    "revision": "be5411fca077f0a984a4",
+    "url": "/tinsmith/js/feature10.d61e4f26.js"
+  },
+  {
+    "revision": "a63e689b3a96effe2c60",
+    "url": "/tinsmith/js/feature11.1dde572d.js"
+  },
+  {
+    "revision": "395e36945dce340c6460",
+    "url": "/tinsmith/js/feature12.1de4b427.js"
+  },
+  {
+    "revision": "ae0c8df69784ae663766",
+    "url": "/tinsmith/js/feature13.eb7ad378.js"
+  },
+  {
+    "revision": "a0069604c43ad60db885",
+    "url": "/tinsmith/js/feature14.985e3195.js"
+  },
+  {
+    "revision": "814c5f4e3cbcf6c449d9",
+    "url": "/tinsmith/js/feature15.956c814e.js"
+  },
+  {
+    "revision": "1a76df21bd42fdd316c0",
+    "url": "/tinsmith/js/feature16.81a03bdf.js"
+  },
+  {
+    "revision": "54c53d6800896f829139",
+    "url": "/tinsmith/js/feature17.1467dbc2.js"
+  },
+  {
+    "revision": "e45cf85762d12dbefaa1",
+    "url": "/tinsmith/js/feature18.5a05fda6.js"
+  },
+  {
+    "revision": "1de4b79e9cd5fa7312a4",
+    "url": "/tinsmith/js/feature19.f85f15a3.js"
+  },
+  {
+    "revision": "e4615aad4d5308d246d9",
+    "url": "/tinsmith/js/feature20.f8a8570b.js"
+  },
+  {
+    "revision": "57d9b958d4bd370f82e2",
+    "url": "/tinsmith/js/feature21.a91dea40.js"
+  },
+  {
+    "revision": "9e1994a6d49fc19d1112",
+    "url": "/tinsmith/js/feature22.82a82e93.js"
+  },
+  {
+    "revision": "0ff6ab4231ddc3728108",
+    "url": "/tinsmith/js/feature23.5e0f1082.js"
+  },
+  {
+    "revision": "41747f215d798e32f022",
+    "url": "/tinsmith/js/feature24.10ee092e.js"
+  },
+  {
+    "revision": "2591127036391ca0e57f",
+    "url": "/tinsmith/js/feature25.8aadd3cb.js"
+  },
+  {
+    "revision": "22e4e1f93b47925162b4",
+    "url": "/tinsmith/js/feature26.45aeb019.js"
+  },
+  {
+    "revision": "2822866a333036bb8062",
+    "url": "/tinsmith/js/feature27.6c11eca4.js"
+  },
+  {
+    "revision": "561aa41ea959075719d6",
+    "url": "/tinsmith/js/feature28.6d5b065b.js"
+  },
+  {
+    "revision": "e2e331ba3a0ba2c429dd",
+    "url": "/tinsmith/js/feature29.8820f5bb.js"
+  },
+  {
+    "revision": "bd530b592b11d2c865ef",
+    "url": "/tinsmith/js/feature30.83ea8982.js"
+  },
+  {
+    "revision": "d53131d0668b28174299",
+    "url": "/tinsmith/js/feature31.85248633.js"
+  },
+  {
+    "revision": "9c8bbaa7888535c34a75",
+    "url": "/tinsmith/js/feature32.f43ff170.js"
+  },
+  {
+    "revision": "5387add1478bcded630e",
+    "url": "/tinsmith/js/feature33.2f47ff07.js"
+  },
+  {
+    "revision": "8c42a8fd13187cd9a6d6",
+    "url": "/tinsmith/js/feature34.8c8ecafd.js"
+  },
+  {
+    "revision": "77e8aad1520cd81369ce",
+    "url": "/tinsmith/js/feature35.3aa917af.js"
+  },
+  {
+    "revision": "57f1aeaddf6a7b7286a2",
+    "url": "/tinsmith/js/feature39.143700de.js"
+  },
+  {
+    "revision": "5e1366939fba5c43b76f",
+    "url": "/tinsmith/js/feature40.01e4a2e2.js"
+  },
+  {
+    "revision": "b5147624d29a0bd6c20e",
+    "url": "/tinsmith/js/feature41.8e46a582.js"
+  },
+  {
+    "revision": "eddac1407e75f33bbcbc",
+    "url": "/tinsmith/js/feature43.69f9fe85.js"
+  },
+  {
+    "revision": "d8c9256d49494a73aa56",
+    "url": "/tinsmith/js/feature44.c9697616.js"
+  },
+  {
+    "revision": "8145fe35efd644c69b05",
+    "url": "/tinsmith/js/feature45.f28e05c0.js"
+  },
+  {
+    "revision": "fcb4b9f8dab0071a2294",
+    "url": "/tinsmith/js/feature46.58ec1bbc.js"
+  },
+  {
+    "revision": "d7bed20831be5e0f2de4",
+    "url": "/tinsmith/js/feature47.a54ecd40.js"
+  },
+  {
+    "revision": "d20e7ac0b0cd82334d85",
+    "url": "/tinsmith/js/feature48.a30a33ad.js"
+  },
+  {
+    "revision": "0b5901a944d4e2285441",
+    "url": "/tinsmith/js/feature49.3770e9a3.js"
+  },
+  {
+    "revision": "f3fce87eba6c32be7ba4",
+    "url": "/tinsmith/js/feature50.29a1c5fd.js"
+  },
+  {
+    "revision": "6bd7a9b48da7efa6d55e",
+    "url": "/tinsmith/js/feature51.e82e9999.js"
+  },
+  {
+    "revision": "a634126895aaedd680db",
+    "url": "/tinsmith/js/feature52.0f7e2eda.js"
+  },
+  {
+    "revision": "712b4c72b88b2d86ae9d",
+    "url": "/tinsmith/js/feature53.eab6883d.js"
+  },
+  {
+    "revision": "bc65c037a2072155b057",
+    "url": "/tinsmith/js/feature54.570b4576.js"
+  },
+  {
+    "revision": "612798da93b0792ba3d6",
+    "url": "/tinsmith/js/feature55.5b879af5.js"
+  },
+  {
+    "revision": "656eee09f71689b2dc3b",
+    "url": "/tinsmith/js/feature56.8502a46a.js"
+  },
+  {
+    "revision": "da60140cc54177102c6d",
+    "url": "/tinsmith/js/feature57.d5eb70df.js"
+  },
+  {
+    "revision": "383d94e0d1cdc084f2e3",
+    "url": "/tinsmith/js/information.728c116e.js"
+  },
+  {
+    "revision": "2d004652d3ff35624365731871a2bb47",
+    "url": "/tinsmith/main.wasm"
+  },
+  {
+    "revision": "55499c0f87eeb9deaf77a16029c5440b",
+    "url": "/tinsmith/manifest.json"
+  },
+  {
+    "revision": "fa1ded1ed7c11438a9b0385b1e112850",
+    "url": "/tinsmith/robots.txt"
+  },
+  {
+    "revision": "43591209d3088ca554e3b1e46186fd16",
+    "url": "/tinsmith/wasm_exec.js"
+  }
+]);
