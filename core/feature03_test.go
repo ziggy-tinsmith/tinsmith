5305791d96c076fd745244e3d30f9ef4d23a8ad5
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:38 +0100

    Refactored core calculations for feature 3

--- core/feature03_test.go
+++ core/feature03_test.go
@@ -13,5 +13,5 @@ var (
 func Test01Feature03(t *testing.T) {
 	arguments := map[string]interface{}{"K": 200, "G": 300, "D": 100}
 	expected := map[string]interface{}{"R": 250, "A": 57.30}
-	genericTester(t, feature03(arguments), expected, feature02Names, feature02Accuracies, feature02Types)
+	genericTester(t, feature03(arguments), expected, feature03Names, feature03Accuracies, feature03Types)
 }
