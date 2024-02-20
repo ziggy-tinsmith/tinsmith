280d4de6b7932a4edae915a6dd5e731341589afe
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:40 +0100

    Added core calculations for feature 22

---
+++ core/feature22.go
@@ -0,0 +1,66 @@
+package main
+
+import "math"
+
+func feature22(arguments map[string]interface{}) map[string]interface{} {
+	parameters := extractParameters(arguments, []string{"D", "A", "K", "G", "L", "J", "V", "T"})
+	if parameters == nil {
+		return nil
+	}
+	result := make(map[string]interface{})
+	D := getParameter(parameters, "D")
+	A := getParameter(parameters, "A")
+	K := getParameter(parameters, "K")
+	G := getParameter(parameters, "G")
+	L := getParameter(parameters, "L")
+	J := getParameter(parameters, "J")
+	V := getParameter(parameters, "V")
+	T := getParameter(parameters, "T")
+	Rad := math.Pi / 180.0
+	U := 30.0
+	if D > 300.0 {
+		U = 40.0
+	}
+	r := D / 2.0
+	N := K / 2.0
+	M := G / 2.0
+	B := math.Atan((M-N)/L) / Rad
+	Y := 1.0 / math.Tan(B*Rad)
+	result["S"] = 2.0 * math.Pi * r / T
+	result["L"] = []interface{}{
+		2.0 * math.Pi * r,
+		2.0*math.Pi*r + U,
+	}
+	A = A + 0.0001
+	theta := A - B
+	E := N*Y + J
+	G = E * math.Tan(B*Rad) * (1.0 / math.Sin(theta*Rad))
+	K = r * (1.0 / math.Tan(A*Rad))
+	L = G + K
+	if A > 90 {
+		L = G - K
+	}
+	p := make([]interface{}, 0)
+	for X := 0.0; X <= T; X += 1.0 {
+		F := 180.0 - X*(360.0/T)
+		W := r*math.Sin(F*Rad) - V
+		I := r * (1.0 / math.Tan(theta*Rad)) * math.Cos(F*Rad)
+		H := E + r*(1.0/math.Sin(A*Rad))*math.Cos(B*Rad)*math.Cos(F*Rad)
+		R := H * math.Tan(B*Rad)
+		K := math.Asin(W/R) / Rad
+		G := math.Asin(W/M) / Rad
+		O := r * math.Cos(K*Rad)
+		U := M * math.Cos(G*Rad)
+		S := R - O
+		P := M*Y - H
+		C := math.Atan((U-O)/P) / Rad
+		Z := S / (math.Tan(theta*Rad) + math.Tan(C*Rad))
+		if V == 0.0 && F < 0.0 {
+			break
+		}
+		Q := L + I - (1.0/math.Sin(theta*Rad))*math.Sqrt(math.Pow(math.Tan(B*Rad), 2.0)*math.Pow(H+Z, 2.0)-math.Pow(W, 2.0))
+		p = append(p, Q)
+	}
+	result["Points"] = p
+	return result
+}
---
+++ core/feature22_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+var (
+	feature22Names      = []string{"S", "L", "Points"}
+	feature22Accuracies = []string{"%.1f", "%.0f", "%.0f"}
+	feature22Types      = []bool{true, false, false}
+)
+
+func Test01feature22(t *testing.T) {
+	arguments := map[string]interface{}{"D": 50, "A": 80, "K": 180, "G": 200, "L": 1000, "J": 50, "V": 2, "T": 10}
+	expected := map[string]interface{}{"S": 15.7, "L": []interface{}{157, 187}, "Points": []interface{}{0, 2, 6, 8, 9, 9, 9, 9, 7, 2, 0}}
+	genericTester(t, feature22(arguments), expected, feature22Names, feature22Accuracies, feature22Types)
+}
+
+func Test02feature22(t *testing.T) {
+	arguments := map[string]interface{}{"D": 400, "A": 100, "K": 900, "G": 1000, "L": 2000, "J": 100, "V": 10, "T": 5}
+	expected := map[string]interface{}{"S": 251.3, "L": []interface{}{1257, 1297}, "Points": []interface{}{72, 85, 21, 26, 95, 72}}
+	genericTester(t, feature22(arguments), expected, feature22Names, feature22Accuracies, feature22Types)
+}
