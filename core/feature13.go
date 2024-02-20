407f98441c7bcd6baf44210a1701986b0c4ef7c2
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:39 +0100

    Refactored core calculations for feature 13

--- core/feature13.go
+++ core/feature13.go
@@ -4,39 +4,54 @@ import (
 	"math"
 )
 
-func feature13(kk, gg, ll, zz, ss, ee float64) (llll, ffkk1, ffkk2, ffgg1, ffgg2, ott, odd, orr, orrkk, orrgg, occkk, occgg float64) {
+func feature13(arguments map[string]interface{}) map[string]interface{} {
+	parameters := extractParameters(arguments, []string{"K", "G", "L", "Z", "S", "E"})
+	if parameters == nil {
+		return nil
+	}
+	K := getParameter(parameters, "K")
+	G := getParameter(parameters, "G")
+	L := getParameter(parameters, "L")
+	Z := getParameter(parameters, "Z")
+	S := getParameter(parameters, "S")
+	E := getParameter(parameters, "E")
+	result := make(map[string]interface{})
 	Rad := math.Pi / 180.0
-	r := kk / 2.0
-	rr := gg / 2.0
-	bb := math.Atan((rr-r)/ll) / Rad
-	vv := 1.0 / math.Sin(bb*Rad)
-	// jj := 1.0 / math.Sin(bb*Rad)
-	yy := r - zz
-	ii := rr - ss
-	uu := math.Acos(yy/r) / Rad
-	oo := math.Acos(ii/rr) / Rad
-	aa := math.Atan((ii-yy)/ll) / Rad
-	ww := 1.0 / math.Cos(aa*Rad)
-	cc := r * math.Sin(uu*Rad)
-	dd := rr * math.Sin(oo*Rad)
-	ff := math.Atan((dd-cc)/(ll*ww)) / Rad
-	tt := 1.0 / math.Cos(ff*Rad)
-	nn := r - ee*math.Sin(bb*Rad)
-	mm := rr + ee*math.Sin(bb*Rad)
-	hh := (oo - uu) * math.Sin(bb*Rad)
-	pp := (180.0 - uu) * math.Sin(bb*Rad)
-	qq := (180.0 - oo) * math.Sin(bb*Rad)
-	llll = ll * ww
-	ffkk1 = cc
-	ffkk2 = 2.0 * cc
-	ffgg1 = dd
-	ffgg2 = 2.0 * dd
-	ott = ll * ww * tt
-	odd = math.Sqrt(math.Pow(dd+cc, 2.0) + math.Pow(ll*ww, 2.0))
-	orr = 2.0 * r * vv * math.Sin((hh/2.0)*Rad)
-	orrkk = nn * vv
-	orrgg = mm * vv
-	occkk = 2.0 * nn * vv * math.Sin((pp/2.0)*Rad)
-	occgg = 2.0 * mm * vv * math.Sin((qq/2.0)*Rad)
-	return
+	r := K / 2.0
+	R := G / 2.0
+	B := math.Atan((R-r)/L) / Rad
+	V := 1.0 / math.Sin(B*Rad)
+	// J := 1.0 / math.Sin(B*Rad)
+	Y := r - Z
+	I := R - S
+	U := math.Acos(Y/r) / Rad
+	O := math.Acos(I/R) / Rad
+	A := math.Atan((I-Y)/L) / Rad
+	W := 1.0 / math.Cos(A*Rad)
+	C := r * math.Sin(U*Rad)
+	D := R * math.Sin(O*Rad)
+	F := math.Atan((D-C)/(L*W)) / Rad
+	T := 1.0 / math.Cos(F*Rad)
+	N := r - E*math.Sin(B*Rad)
+	M := R + E*math.Sin(B*Rad)
+	H := (O - U) * math.Sin(B*Rad)
+	P := (180.0 - U) * math.Sin(B*Rad)
+	Q := (180.0 - O) * math.Sin(B*Rad)
+	result["L"] = L * W
+	result["FK"] = []interface{}{
+		C,
+		2.0 * C,
+	}
+	result["FG"] = []interface{}{
+		D,
+		2.0 * D,
+	}
+	result["T"] = L * W * T
+	result["D"] = math.Sqrt(math.Pow(D+C, 2.0) + math.Pow(L*W, 2.0))
+	result["R"] = 2.0 * r * V * math.Sin((H/2.0)*Rad)
+	result["RK"] = N * V
+	result["RG"] = M * V
+	result["CK"] = 2.0 * N * V * math.Sin((P/2.0)*Rad)
+	result["CG"] = 2.0 * M * V * math.Sin((Q/2.0)*Rad)
+	return result
 }
--- core/feature13_test.go
+++ core/feature13_test.go
@@ -4,46 +4,14 @@ import (
 	"testing"
 )
 
-func feature13Tester(t *testing.T, kk, gg, ll, zz, ss, ee float64, llllOK, ffkk1OK, ffkk2OK, ffgg1OK, ffgg2OK, ottOK, oddOK, orrOK, orrkkOK, orrggOK, occkkOK, occggOK float64) {
-	llll, ffkk1, ffkk2, ffgg1, ffgg2, ott, odd, orr, orrkk, orrgg, occkk, occgg := feature13(kk, gg, ll, zz, ss, ee)
-	if !equalsFormatted(llll, llllOK, "%.2f") {
-		t.Errorf("The value 'llll' is incorrect (got: %v, expected: %v)!", llll, llllOK)
-	}
-	if !equalsFormatted(ffkk1, ffkk1OK, "%.2f") {
-		t.Errorf("The value 'ffkk1' is incorrect (got: %v, expected: %v)!", ffkk1, ffkk1OK)
-	}
-	if !equalsFormatted(ffkk2, ffkk2OK, "%.2f") {
-		t.Errorf("The value 'ffkk2' is incorrect (got: %v, expected: %v)!", ffkk2, ffkk2OK)
-	}
-	if !equalsFormatted(ffgg1, ffgg1OK, "%.2f") {
-		t.Errorf("The value 'ffgg1' is incorrect (got: %v, expected: %v)!", ffgg1, ffgg1OK)
-	}
-	if !equalsFormatted(ffgg2, ffgg2OK, "%.2f") {
-		t.Errorf("The value 'ffgg2' is incorrect (got: %v, expected: %v)!", ffgg2, ffgg2OK)
-	}
-	if !equalsFormatted(ott, ottOK, "%.2f") {
-		t.Errorf("The value 'ott' is incorrect (got: %v, expected: %v)!", ott, ottOK)
-	}
-	if !equalsFormatted(odd, oddOK, "%.2f") {
-		t.Errorf("The value 'odd' is incorrect (got: %v, expected: %v)!", odd, oddOK)
-	}
-	if !equalsFormatted(orr, orrOK, "%.2f") {
-		t.Errorf("The value 'orr' is incorrect (got: %v, expected: %v)!", orr, orrOK)
-	}
-	if !equalsFormatted(orrkk, orrkkOK, "%.2f") {
-		t.Errorf("The value 'orrkk' is incorrect (got: %v, expected: %v)!", orrkk, orrkkOK)
-	}
-	if !equalsFormatted(orrgg, orrggOK, "%.2f") {
-		t.Errorf("The value 'orrgg' is incorrect (got: %v, expected: %v)!", orrgg, orrggOK)
-	}
-	if !equalsFormatted(occkk, occkkOK, "%.2f") {
-		t.Errorf("The value 'occkk' is incorrect (got: %v, expected: %v)!", occkk, occkkOK)
-	}
-	if !equalsFormatted(occgg, occggOK, "%.2f") {
-		t.Errorf("The value 'occgg' is incorrect (got: %v, expected: %v)!", occgg, occggOK)
-	}
-}
+var (
+	feature13Names      = []string{"L", "FK", "FG", "T", "D", "R", "RK", "RG", "CK", "CG"}
+	feature13Accuracies = []string{"%.2f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f"}
+	feature13Types      = []bool{true, false, false, true, true, true, true, true, true, true}
+)
 
 func Test01Feature13(t *testing.T) {
-	feature13Tester(t, 200, 400, 100, 60, 80, 12, 128.06, 91.65, 183.30, 160.00, 320.00, 145.16, 282.36, -23.17, 129.42, 294.84, 166.92, 415.92)
+	arguments := map[string]interface{}{"K": 200, "G": 400, "L": 100, "Z": 60, "S": 80, "E": 12}
+	expected := map[string]interface{}{"L": 128.06, "FK": []interface{}{91.65, 183.30}, "FG": []interface{}{160.00, 320.00}, "T": 145.16, "D": 282.36, "R": -23.17, "RK": 129.42, "RG": 294.84, "CK": 166.92, "CG": 415.92}
+	genericTester(t, feature13(arguments), expected, feature13Names, feature13Accuracies, feature13Types)
 }
