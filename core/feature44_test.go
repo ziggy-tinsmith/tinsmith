f41cec8b651ca75e55cfaff7f619878a0352b66a
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:40 +0100

    Added core calculations for feature 44

--- core/feature44_test.go
+++ core/feature44_test.go
@@ -13,3 +13,9 @@ func Test01feature44(t *testing.T) {
 	expected := map[string]interface{}{"S": 78.5, "L": []interface{}{628, 658}, "PointsB": []interface{}{100, 129, 200, 200, 200}, "PointsA": []interface{}{200, 229, 300}}
 	genericTester(t, feature44(arguments), expected, feature44Names, feature44Accuracies, feature44Types)
 }
+
+func Test02feature44(t *testing.T) {
+	arguments := map[string]interface{}{"D": 400, "L": 400, "V": 400, "T": 6, "LL": 600}
+	expected := map[string]interface{}{"S": 209.4, "L": []interface{}{1257, 1297}, "PointsB": []interface{}{200, 300, 400}, "PointsA": []interface{}{400, 500}}
+	genericTester(t, feature44(arguments), expected, feature44Names, feature44Accuracies, feature44Types)
+}
