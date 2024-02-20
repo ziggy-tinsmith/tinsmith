c45a6c272dcc8417e9f266a0e1b384cbb7833521
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:39 +0100

    Corrected testing of core calculations for feature 6

--- core/feature06_test.go
+++ core/feature06_test.go
@@ -12,12 +12,12 @@ var (
 
 func Test01Feature06(t *testing.T) {
 	arguments := map[string]interface{}{"D": 200, "A": 90, "T": 12}
-	expected := map[string]interface{}{"S": 52.4, "L": []float64{628, 658}, "Points": []float64{0, 13, 50, 100, 150, 187, 200}}
+	expected := map[string]interface{}{"S": 52.4, "L": []interface{}{628, 658}, "Points": []interface{}{0, 13, 50, 100, 150, 187, 200}}
 	genericTester(t, feature06(arguments), expected, feature06Names, feature06Accuracies, feature06Types)
 }
 
 func Test02Feature06(t *testing.T) {
 	arguments := map[string]interface{}{"D": 400, "A": 90, "T": 16}
-	expected := map[string]interface{}{"S": 78.5, "L": []float64{1257, 1297}, "Points": []float64{0, 15, 59, 123, 200, 277, 341, 385, 400}}
+	expected := map[string]interface{}{"S": 78.5, "L": []interface{}{1257, 1297}, "Points": []interface{}{0, 15, 59, 123, 200, 277, 341, 385, 400}}
 	genericTester(t, feature06(arguments), expected, feature06Names, feature06Accuracies, feature06Types)
 }
