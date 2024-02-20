989364e352df481f4802375ddf74208db00805df
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:36 +0100

    Preparing for GitHub Pages deployment

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -3,5 +3,6 @@ const packageJson = fs.readFileSync("./package.json");
 process.env.VUE_APP_VERSION = JSON.parse(packageJson).version || "0.0.0";
 
 module.exports = {
+  publicPath: "/tinsmith/",
   transpileDependencies: ["vuetify"]  // Related to: https://github.com/vuetifyjs/vuetify/issues/8279
 };
