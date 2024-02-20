d4159d1ed483d47f8d05dc9e034fa916839b551d
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:39 +0100

    Corrected testing of core calculations for feature 15

--- core/feature15_test.go
+++ core/feature15_test.go
@@ -12,12 +12,12 @@ var (
 
 func Test01Feature15(t *testing.T) {
 	arguments := map[string]interface{}{"D": 200, "G": 400, "V": 100, "T": 10}
-	expected := map[string]interface{}{"S": 62.8, "L": []interface{}{628, 658}, "Points": []interface{}{0, 1, 12, 49, 115, 200}, "B": []interface{}{0, 0, 0, 0, 0, 0}, "H": []interface{}{0, 59, 95}}
+	expected := map[string]interface{}{"S": 62.8, "L": []interface{}{628, 658}, "Points": []interface{}{0, 1, 12, 49, 115, 200}, "B": []interface{}{105, 86, 34, -38, -121, -209}, "H": []interface{}{0, 59, 95}}
 	genericTester(t, feature15(arguments), expected, feature15Names, feature15Accuracies, feature15Types)
 }
 
 func Test02Feature15(t *testing.T) {
 	arguments := map[string]interface{}{"D": 350, "G": 600, "V": 100, "T": 10}
-	expected := map[string]interface{}{"S": 110.0, "L": []interface{}{1100, 1140}, "Points": []interface{}{10, 3, 4, 43, 122, 180}, "B": []interface{}{0, 0, 0, 0, 0, 0}, "H": []interface{}{0, 103, 166}}
+	expected := map[string]interface{}{"S": 110.0, "L": []interface{}{1100, 1140}, "Points": []interface{}{10, 3, 4, 43, 122, 180}, "B": []interface{}{178, 144, 56, -60, -179, -246}, "H": []interface{}{0, 103, 166}}
 	genericTester(t, feature15(arguments), expected, feature15Names, feature15Accuracies, feature15Types)
 }
