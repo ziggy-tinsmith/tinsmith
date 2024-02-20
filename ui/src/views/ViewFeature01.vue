826961612c21a19e8e4c135765a8185792cfedbb
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:37 +0100

    Added visual aid

--- ui/src/views/ViewFeature01.vue
+++ ui/src/views/ViewFeature01.vue
@@ -1,7 +1,7 @@
 <template>
   <v-main>
     <v-card class="d-flex flex-column ma-0 pa-0 fill-height" flat>
-      <v-card class="mx-4 mt-4 mb-2 px-4 py-2" align="center">
+      <v-card class="mx-2 mt-3 mb-1 px-4 py-0" align="center">
         <v-row>
           <v-col>
             <v-text-field v-model="dd" :min="0" dense label="D"></v-text-field>
@@ -16,11 +16,36 @@
             <v-text-field v-model="ee" :min="0" dense label="E"></v-text-field>
           </v-col>
           <v-col>
-            <v-text-field v-model="tt" :min="0" dense label="T"></v-text-field
-          ></v-col>
+            <v-text-field v-model="tt" :min="0" dense label="T"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-dialog v-model="help" persistent>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon v-bind="attrs" v-on="on">
+                  <v-icon>mdi-help-circle-outline</v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn icon @click="help = false">
+                    <v-icon>mdi-close</v-icon>
+                  </v-btn>
+                </v-card-actions>
+                <v-card-text align="center">
+                  <v-img
+                    max-height="70vh"
+                    max-width="80vh"
+                    contain
+                    :src="require('@/assets/feature01.png')"
+                  />
+                </v-card-text>
+              </v-card>
+            </v-dialog>
+          </v-col>
         </v-row>
       </v-card>
-      <v-card class="mx-4 mt-2 mb-4 px-4 py-2 flex-grow-1 d-flex align-stretch">
+      <v-card class="mx-2 mt-1 mb-3 px-4 py-0 flex-grow-1 d-flex align-stretch">
         <v-container>
           <v-row>
             <v-col cols="1">‡</v-col>
@@ -72,6 +97,7 @@ export default Vue.extend({
     ss: "",
     ee: "",
     tt: "",
+    help: false,
   }),
 
   methods: {
