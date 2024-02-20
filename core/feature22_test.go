c7f30c253ec70ef1e59cd6faac7b9af028a2e3e7
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:40 +0100

    Corrected testing of core calculations for feature 22

--- core/feature22_test.go
+++ core/feature22_test.go
@@ -10,12 +10,12 @@ var (
 
 func Test01feature22(t *testing.T) {
 	arguments := map[string]interface{}{"D": 50, "A": 80, "K": 180, "G": 200, "L": 1000, "J": 50, "V": 2, "T": 10}
-	expected := map[string]interface{}{"S": 15.7, "L": []interface{}{157, 187}, "Points": []interface{}{0, 2, 6, 8, 9, 9, 9, 9, 7, 2, 0}}
+	expected := map[string]interface{}{"S": 15.7, "L": []interface{}{157, 187}, "Points": []interface{}{0, 2, 6, 8, 9, 9, 10, 10, 7, 2, 0}}
 	genericTester(t, feature22(arguments), expected, feature22Names, feature22Accuracies, feature22Types)
 }
 
 func Test02feature22(t *testing.T) {
 	arguments := map[string]interface{}{"D": 400, "A": 100, "K": 900, "G": 1000, "L": 2000, "J": 100, "V": 10, "T": 5}
-	expected := map[string]interface{}{"S": 251.3, "L": []interface{}{1257, 1297}, "Points": []interface{}{72, 85, 21, 26, 95, 72}}
+	expected := map[string]interface{}{"S": 251.3, "L": []interface{}{1257, 1297}, "Points": []interface{}{71, 84, 20, 25, 94, 71}}
 	genericTester(t, feature22(arguments), expected, feature22Names, feature22Accuracies, feature22Types)
 }
