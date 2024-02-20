5656a0a58e54fb9110702c8978927fc8fa439b5a
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:42 +0100

    Corrected tests

--- core/feature27_test.js
+++ core/feature27_test.js
@@ -1,6 +1,6 @@
 var feature27Names = ["KS", "GS", "R1", "R2", "L1", "L2"];
 var feature27Accuracies = [1, 1, 0, 0, 0, 0];
-var feature27Types = [true, true, false, true, true, false];
+var feature27Types = [true, true, false, false, false, false];
 
 function test01feature27() {
   var arguments = {"K": 100, "G": 150, "L": 100, "V": 20, "Z": 12, "T": 8};
--- core/feature33_test.js
+++ core/feature33_test.js
@@ -4,7 +4,7 @@ var feature33Types = [true, true, true, true, true, false, true, true];
 
 function test01feature33() {
   var arguments = {"D": 200, "I": 10, "K": 20, "S": 8, "E": 2, "T": 6};
-  var expected = {"SS": 2, "S": 17.5, "CL": 112, "R": 134, "H": 29, "Points": [6, 13, 21, 28, 34, 41, 48], "Area": 0.04, "VL": 0.39};
+  var expected = {"SS": 2, "S": 17.5, "CL": 112, "R": 134, "H": 29, "Points": [6, 13, 21, 28, 34, 41, 48], "Area": 0.04, "Volume": 0.39};
   genericTester(feature33_core(arguments), expected, feature33Names, feature33Accuracies, feature33Types);
 }
 
