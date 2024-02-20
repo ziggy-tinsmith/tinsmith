0fa971ef4d4c0607182945d494a81aa0dc605065
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:42 +0100

    Corrected tests

--- core/feature09a_test.js
+++ core/feature09a_test.js
@@ -5,7 +5,7 @@ var feature09ATypes = [true, true, true, true, false, false, false];
 function test01Feature09A() {
   var arguments = {"D": 200, "A": 90, "L": 500, "T": 12};
   var expected = {"F1": 99, "F2": 199, "H": 508, "S": 24.5, "Points": [518, 513, 509, 505, 502, 501, 500], "P1L": [25, 339], "P2L": [182, 182]};
-  genericTester(feature09A_core(arguments), expected, feature09ANames, feature09AAccuracies, feature09ATypes);
+  genericTester(feature09a_core(arguments), expected, feature09ANames, feature09AAccuracies, feature09ATypes);
 }
 
 test01Feature09A();
@@ -13,7 +13,7 @@ test01Feature09A();
 function test02Feature09A() {
   var arguments = {"D": 400, "A": 90, "L": 600, "T": 16};
   var expected = {"F1": 167, "F2": 334, "H": 607, "S": 24.7, "Points": [629, 623, 617, 612, 608, 605, 602, 601, 600], "P1L": [136, 765], "P2L": [451, 451]};
-  genericTester(feature09A_core(arguments), expected, feature09ANames, feature09AAccuracies, feature09ATypes);
+  genericTester(feature09a_core(arguments), expected, feature09ANames, feature09AAccuracies, feature09ATypes);
 }
 
 test02Feature09A();
--- core/feature09b_test.js
+++ core/feature09b_test.js
@@ -5,7 +5,7 @@ var feature09BTypes = [false, false, false, true, false, false, false, false];
 function test01Feature09B() {
   var arguments = {"D": 200, "A": 90};
   var expected = {"F": [99, 199], "R": [147, 294], "D": [48, 95], "T": 10, "L": [533, 563], "L1": [110, 140], "L2": [267, 297], "L3": [424, 454]};
-  genericTester(feature09B_core(arguments), expected, feature09BNames, feature09BAccuracies, feature09BTypes);
+  genericTester(feature09b_core(arguments), expected, feature09BNames, feature09BAccuracies, feature09BTypes);
 }
 
 test01Feature09B();
@@ -13,7 +13,7 @@ test01Feature09B();
 function test02Feature09B() {
   var arguments = {"D": 400, "A": 60};
   var expected = {"F": [143, 286], "R": [159, 318], "D": [16, 33], "T": 155, "L": [1224, 1264], "L1": [298, 338], "L2": [612, 652], "L3": [926, 966]};
-  genericTester(feature09B_core(arguments), expected, feature09BNames, feature09BAccuracies, feature09BTypes);
+  genericTester(feature09b_core(arguments), expected, feature09BNames, feature09BAccuracies, feature09BTypes);
 }
 
 test02Feature09B();
