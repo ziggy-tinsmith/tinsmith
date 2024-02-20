f4e1169137ea937aff93bd663f9825bb906f15f0
ziggy-tinsmith <160263225+ziggy-tinsmith@users.noreply.github.com>
2024-02-20 12:24:38 +0100

    Corrected testing of core calculations for feature 1

--- core/feature01_test.go
+++ core/feature01_test.go
@@ -12,12 +12,12 @@ var (
 
 func Test01Feature01(t *testing.T) {
 	arguments := map[string]interface{}{"D": 200, "R": 500, "S": 5, "E": 10, "T": 12}
-	expected := map[string]interface{}{"S": 52.4, "L": []float64{628, 658}, "Points": []float64{73, 75, 81, 89, 97, 103, 105}, "Area": 0.49}
+	expected := map[string]interface{}{"S": 52.4, "L": []interface{}{628, 658}, "Points": []interface{}{73, 75, 81, 89, 97, 103, 105}, "Area": 0.49}
 	genericTester(t, feature01(arguments), expected, feature01Names, feature01Accuracies, feature01Types)
 }
 
 func Test02Feature01(t *testing.T) {
 	arguments := map[string]interface{}{"D": 400, "R": 600, "S": 8, "E": 20, "T": 16}
-	expected := map[string]interface{}{"S": 78.5, "L": []float64{1257, 1297}, "Points": []float64{59, 61, 65, 72, 79, 89, 93, 97, 99}, "Area": 1.18}
+	expected := map[string]interface{}{"S": 78.5, "L": []interface{}{1257, 1297}, "Points": []interface{}{59, 61, 65, 72, 79, 89, 93, 97, 99}, "Area": 1.18}
 	genericTester(t, feature01(arguments), expected, feature01Names, feature01Accuracies, feature01Types)
 }
